232f6e0ee32753cd6607694ab8e7aa24e7c3ccf9
Racheal Njuguna <rachaelnjuguna418@gmail.com>
2023-02-17 11:05:23 -0800

    3alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 'A';
+
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
