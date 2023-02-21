6914a155d0df2504fb3fc9ea073455ed835c19c5
Racheal Njuguna <rachaelnjuguna418@gmail.com>
2023-02-21 14:15:01 -0800

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-/*
- * Function prototypes
- */
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -18,4 +11,3 @@ int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 
-#endif /* _MAIN_H_ */
