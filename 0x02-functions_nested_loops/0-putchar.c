44214cca92837c585e6f689c46db673e611b53ca
Racheal Njuguna <rachaelnjuguna418@gmail.com>
2023-02-21 15:03:29 -0800

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
- * main - a program that prints _putchar, followed by a new line
- *
- * Return = 0;
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
