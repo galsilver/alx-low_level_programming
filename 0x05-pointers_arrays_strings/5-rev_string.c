539df3197e7be837f28335aa1d55e99fe9472d7b
Racheal Njuguna <rachaelnjuguna418@gmail.com>
2023-02-27 11:52:47 -0800

    vs string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - puts a string to stdout reversed
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int up, down;
+
+	while (*(s + len) != '\0')
+		len++;
+	len--;
+
+	for (up = 0, down = len; up < down; up++, down--)
+	{
+		char temp = *(s + up);
+		*(s + up) = *(s + down);
+		*(s + down) = temp;
+	}
+}
