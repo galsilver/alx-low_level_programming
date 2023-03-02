e5bfa9b2ee9f9c7e8b39bb96dfdbbd04f82d5eaf
Racheal Njuguna <rachaelnjuguna418@gmail.com>
2023-03-02 12:43:56 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int length, j;
-	length = 0;
-	while (dest[length] != '\0')
+	char *s = dest;
+
+	while (*dest != '\0')
 	{
-		length++;
+		dest++;
 	}
-	for (j = 0; src[j] != '\0'; j++, length++)
+
+	while (*src != '\0')
 	{
-		dest[length] = src[j];
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	dest[length] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (s);
 }
